0b20d51342160d5b42084335f72dd37b513e548e
Renan Lopes Obregon <lopesobregon.renan5@gmail.com>
2026-08-13 15:49:41 -0400

    update geral agora possivel de um assinante.

--- ApiBridge/Controllers/SyncController.cs
+++ ApiBridge/Controllers/SyncController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ApiBridge.Models.Dto;
 using ApiBridge.Services;
+using System.Linq.Expressions;
 
 namespace ApiBridge.Controllers
 {
@@ -24,16 +25,48 @@ namespace ApiBridge.Controllers
             return Ok(new { sucess = true, message = "Usuário sincronizado com sucesso!" });
         }
         [HttpPut("users")]
-        public IActionResult AtualizarUsuario([FromBody] SyncUserDto userDto)
+        public async Task<IActionResult> AtualizarUsuarioAsync([FromBody] SyncUserDto userDto)
         {
             ServiceUsers service = new();
+            bool sucess = false;
             if (userDto is null || String.IsNullOrEmpty(userDto.Name))
                 return BadRequest(new { mensagem = "Dados invalidos" });
             else
             {
-                service.UpdateUser(userDto);
+               sucess =  await service.UpdateUser(userDto);
+
             }
-            return Ok(new { sucess = true, message = "Usuario atualizado com sucesso!" });
+            if (sucess)
+                return Ok(new { sucess = true, message = "Usuario atualizado com sucesso!" });
+            else
+                return BadRequest(new { sucess = false, message = $"Erro ao atualizar o usuario {userDto.Name} | ID: {userDto.External_id}" });
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsersAsync()
+        {
+            //get vem do sistema principal para coletar os dados dos usuarios
+            ServiceUsers serviceUsers = new();
+            List<SyncUserDto> syncUserDtos = new();
+            bool sucess = false;
+             sucess = await serviceUsers.GetUsers(syncUserDtos);
+            /* foreach (var usr in syncUserDtos)
+            {
+                Console.WriteLine($"Id:{usr.External_id} | Nome: {usr.Name}");
+            }*/
+            if (sucess)
+                return Ok(new { sucess = true, message = "Usuários sincrocizados!", users = syncUserDtos });
+            else
+                return BadRequest(new { sucess = false, message = "Algo deu errado!", usrs = syncUserDtos });
+        }
+        [HttpDelete("users")]
+        public async Task<IActionResult> DeleteUserAsync([FromBody] SyncUserDto syncUserDto)
+        {
+            ServiceUsers serviceUsers = new();
+            bool sucess =  await serviceUsers.DeleteUser(syncUserDto);
+            if (sucess)
+                return Ok(new { sucess = true, message = $"Usuário {syncUserDto.Name}, {syncUserDto.Email} Deletado com sucesso!" });
+            else
+                return BadRequest(new { sucess = false, message = $"Usuário {syncUserDto.Name}, {syncUserDto.Email} Deletado com sucesso!" });
         }
     }
 }
--- ApiBridge/Services/ServiceUsers.cs
+++ ApiBridge/Services/ServiceUsers.cs
@@ -1,17 +1,52 @@
 ﻿using ApiBridge.Models.Dto;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ApiBridge.Services
 {
     public class ServiceUsers
     {
+        internal async Task<bool> DeleteUser(SyncUserDto syncUserDto)
+        {
+            string urlCamioneiro = "https://gestor-mei-caminhoneiro-d1039.shrd00.internal.goskip.dev/backend/v1/subscribers";
+            HttpClient client = new();
+            HttpRequestMessage httpRequestMessage = new(HttpMethod.Delete, urlCamioneiro)
+            {
+                Content = JsonContent.Create(syncUserDto)
+            };
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage);
+            return response.IsSuccessStatusCode;
+
+        }
+
         internal void Exists(SyncUserDto userDto)
         {
             throw new NotImplementedException();
         }
+
+        internal async Task<bool> GetUsers(List<SyncUserDto> syncUserDtos)
+        {
+            string urlCamioneiro = "https://gestor-mei-caminhoneiro-d1039.shrd00.internal.goskip.dev/backend/v1/subscribers";
+            HttpClient client = new();
+            List<SyncUserDto>? userDtos = new();
+            
+            userDtos = await client.GetFromJsonAsync<List<SyncUserDto>>(urlCamioneiro);
+            if (userDtos is not null && userDtos.Any())
+            {
+                syncUserDtos.AddRange(userDtos);
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("Algo deu Errado!");
+                return false;
+            }
+        }
+
         // envia o usuario para o sistema de gerenciamento de asinantes de outros sistemas.
         internal async Task SendUser(SyncUserDto userDto)
         {
-            string resultado = String.Empty;
+            string resultado =  System.String.Empty;
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.PostAsJsonAsync("https://gestao-de-empresa-de-sistemas-e4fd0.shrd00.internal.goskip.dev/backend/v1/sync-users", userDto);
             //verificação de status
@@ -32,9 +67,9 @@ namespace ApiBridge.Services
             throw new NotImplementedException();
         }
         //enviar para outro o usuario atualizado
-        internal async Task UpdateUser(SyncUserDto userDto)
+        internal async Task<bool> UpdateUser(SyncUserDto userDto)
         {
-            string resultado = String.Empty;
+            string resultado = System.String.Empty;
             HttpClient client = new HttpClient();
             //post por que o outro sistema espera um método do tipo post
             HttpResponseMessage response = await client.PostAsJsonAsync("https://gestor-mei-caminhoneiro-d1039.shrd00.internal.goskip.dev/backend/v1/sync-users", userDto);
@@ -44,10 +79,12 @@ namespace ApiBridge.Services
                 Console.WriteLine("Usuário enviado com sucesso!");
                 resultado = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"Resultado: {resultado}");
+                return true;
             }
             else
             {
                 Console.WriteLine($"Erro ao enviar usuário: {response.StatusCode}");
+                return false;
             }
 
         }
